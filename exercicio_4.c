5ab5b05cd75b268c727ce2f8b5d30ea57c34c429
Willian Wolensky Kraft <williankraft15@gmail.com>
2026-09-06 18:15:32 -0300

    Adicionar exercicio 4

---
+++ exercicio_4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    
+    float valorcompra;
+    float desconto; 
+    float valorfinal;
+    printf("Digite o valor da sua compra:");
+    scanf("%f",&valorcompra);
+    
+    if (valorcompra >= 300.00){
+    desconto = valorcompra * 0.12;
+    valorfinal = valorcompra - desconto;
+    printf("O valor de seu desconto e de 12%%, no valor de R$ %.2f\n", desconto);
+    printf("Seu valor final foi de: R$ %2.f\n", valorfinal);
+    }
+    
+    else{
+        desconto = valorcompra * 0.05;
+        valorfinal = valorcompra - desconto;
+        printf("O valor de seu desconto e de 5%%, no valor de R$ %.2f\n", desconto);
+        printf("Seu valor final foi de: R$ %2.f\n",valorfinal);
+        
+    }
+    
+    
+    
+    
+    return 0;
+}
