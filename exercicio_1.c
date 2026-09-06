ce9b71e3a120c284cb03c306d6b25432ac2b56ce
Willian Wolensky Kraft <williankraft15@gmail.com>
2026-09-06 17:27:41 -0300

    Corrige exercicio 1 e adiciona exercicio 3

--- exercicio_1.c
+++ exercicio_1.c
@@ -2,13 +2,16 @@
 
 int main() {
 
-  int n1,n2;
-  printf("Digite dois numeros: ");
-  scanf("%d %d", &n1, &n2);
- 
-  int soma = n1 + n2;  
+  int n1;
+  int n2;
+  int soma;
+  printf("Digite um numero:");
+  scanf("%d", &n1);
+  printf("Digite outro numero:");
+  scanf("%d", &n2);
+  soma = n1 + n2;
 
-
+  printf("A soma dos dois numeros e: %d", soma);
 
     return 0;
 }
---
+++ exercicio_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    
+    int n1;
+    
+    printf("Digite um numero:");
+    scanf("%d",&n1);
+    
+    
+    if (n1 % 2 ==0){
+    printf("Se numero e par");
+        
+    
+    }
+    
+    else{
+        printf("Seu numero e impar");
+    
+    }
+    
+    
+    
+    
+    return 0;
+}
